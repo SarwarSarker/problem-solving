13b0baeee0e6005deaa30a46f50fa4f3e4b1e2e6
SarwarSarker <sarwarsarker00@gmail.com>
2024-03-15 23:26:05 +0600

    added solution 2 of 9 Palindrome Number

--- math/9-Palindrome-Number/index.py
+++ math/9-Palindrome-Number/index.py
@@ -1,7 +1,19 @@
-class Solution:
+class Solution1 :
     def isPalindrome(self, x: int) -> bool:
         z = str(x)
         s = z[::-1]
 
         if s == z :
+            return True
+
+class Solution2:
+    def isPalindrome(self, x: int) -> bool:
+        temp=x
+        rev=0
+
+        while(x>0):
+            digit= x%10
+            rev = (rev*10) + digit
+            x = x//10
+        if temp == rev:
             return True
